c8890995b57c60bfa65fcf3595a689fc06b91ea2
Heman <fullyundetectable@outlook.com>
2018-12-13 19:38:03 +0000

    Silly bug fixed (songs were not uploaded)

---
+++ backgroundMusic.js
@@ -0,0 +1,35 @@
+let songs = [
+    {
+        name: "Melody",
+        src: "m"
+    },
+    {
+        name: "Enna Sona",
+        src: "es"
+    },
+    {
+        name: "Despacito",
+        src: "D"
+    },
+
+    {
+        name: "Let her Go",
+        src: "lhg"
+    },
+    {
+        name: "Shape of You",
+        src: 'soy'
+    },
+    {
+        name: "I Took A Pill In Ibiza",
+        src: "pill"
+    },
+];
+
+randomIndex=Math.round(Math.random()*songs.length-1);
+audio=document.createElement('audio');
+audio.src=songs[randomIndex].src +'.mp3';
+audio.autoplay='autoplay';
+audio.loop='loop';
+document.querySelector('header h1').textContent=songs[randomIndex].name;
+document.body.append(audio);
